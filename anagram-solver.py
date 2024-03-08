cac080ae2330544b0a96fa820117800a7cd5af84
kduszak <kamilduszak016@gmail.com>
2024-03-08 16:00:00 +0000

    added thesaurus source

--- anagram-solver.py
+++ anagram-solver.py
@@ -1,5 +1,14 @@
 import pandas as pd
 
-# Creating a list of all unique words
-dictionary = pd.read_csv('dictionary.csv', keep_default_na = False, na_values = ['NaN'])
+# Creating an ordered list of all unique terms from two sources
+dictionary = pd.read_csv('dictionary.csv', converters={'Word' : str})
 words = list(dictionary['Word'].drop_duplicates())
+thesaurus = pd.read_csv('thesaurus.csv', converters={'Word' : str})
+words = words + list(thesaurus['Word'].drop_duplicates())
+for i in range(len(words)):
+    words[i] = words[i].lower()
+words.sort()
+
+
+
+
